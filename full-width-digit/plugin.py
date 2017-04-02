eeba4aaa5b2c432f8f7e4ade1f6e65f84443e2f6
林檎 <apple@apples-MacBook-Pro.local>
2017-04-02 14:16:52 +0800

    Fix Bug on global regex

--- full-width-digit/plugin.py
+++ full-width-digit/plugin.py
@@ -7,20 +7,28 @@ import sys
 def run(bk):
     print('start')
     for (id, href) in bk.text_iter():
+        modified = False
         html = bk.readfile(id)
-        old_html = html
-        html = re.sub("0", "\N{FULLWIDTH DIGIT ZERO}", html)
-        html = re.sub("1", "\N{FULLWIDTH DIGIT ONE}", html)
-        html = re.sub("2", "\N{FULLWIDTH DIGIT TWO}", html)
-        html = re.sub("3", "\N{FULLWIDTH DIGIT THREE}", html)
-        html = re.sub("4", "\N{FULLWIDTH DIGIT FOUR}", html)
-        html = re.sub("5", "\N{FULLWIDTH DIGIT FIVE}", html)
-        html = re.sub("6", "\N{FULLWIDTH DIGIT SIX}", html)
-        html = re.sub("7", "\N{FULLWIDTH DIGIT SEVEN}", html)
-        html = re.sub("8", "\N{FULLWIDTH DIGIT EIGHT}", html)
-        html = re.sub("9", "\N{FULLWIDTH DIGIT NINE}", html)
-        if html != old_html:
-            print("Modifed ", id)
+		html = html.replace("<br/>","")
+        soup = gumbo_bs4.parse(html)
+        ol = sigil_bs4.Tag(name="ol")
+        ol['class'] = "sigil-footnote-content"
+        # br tag  will cause p tag cannot be found
+        for elem in soup.findAll(['p','div','span'], text=re.compile('(\d+)')):
+            modified = True
+            text = elem.string
+            text = re.sub("0", "\N{FULLWIDTH DIGIT ZERO}", text)
+            text = re.sub("1", "\N{FULLWIDTH DIGIT ONE}", text)
+            text = re.sub("2", "\N{FULLWIDTH DIGIT TWO}", text)
+            text = re.sub("3", "\N{FULLWIDTH DIGIT THREE}", text)
+            text = re.sub("4", "\N{FULLWIDTH DIGIT FOUR}", text)
+            text = re.sub("5", "\N{FULLWIDTH DIGIT FIVE}", text)
+            text = re.sub("6", "\N{FULLWIDTH DIGIT SIX}", text)
+            text = re.sub("7", "\N{FULLWIDTH DIGIT SEVEN}", text)
+            text = re.sub("8", "\N{FULLWIDTH DIGIT EIGHT}", text)
+            text = re.sub("9", "\N{FULLWIDTH DIGIT NINE}", text)
+        if modified:
+            print("Modifed File -> ", id)
             bk.writefile(id, html)
     return 0
 
