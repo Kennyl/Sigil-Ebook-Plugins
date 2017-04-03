15e31292277e57c7d3b1e410c92fd86a26a44cd3
林檎 <apple@apples-MacBook-Pro.local>
2017-04-03 17:21:07 +0800

    new zip logic that will contain timestamp

--- zipPlugins.sh
+++ zipPlugins.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
 
-find * -type d -exec zip  -r {}.zip  {} \;
+ymd=$(date +%Y%m%d)
+
+find . -name "plugin.xml" -exec sed -i "s/^<version>.*$/<version>${ymd}<\/version>/"  {} \;
+
+# * because we ignore .git those hidden folder
+find * -type d -exec zip -r {}_v${ymd}.zip {} \;
