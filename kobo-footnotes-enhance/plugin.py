81c639fb4d8efc131291d4f99301174a71405952
林檎 <apple@apples-MacBook-Pro.local>
2017-04-07 00:23:54 +0800

    adaptive to book mistake

--- kobo-footnotes-enhance/plugin.py
+++ kobo-footnotes-enhance/plugin.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-from __future__ import unicode_literals, division, absolute_import, print_function
 import sys
 import re
 from lxml import etree, html
-import os
 
 
 '''
@@ -16,7 +14,7 @@ and （注釋2）<-> section ■注釋 of 2
 def run(bk):
     modified = False
 # all xhtml/html files - moves found notes to end of file, insert a link in the text and link to css in the files with notes
-    step = 1 # 1 for footnote #2 For 校記 #3 for noteref
+    step = 1  # 1 for footnote #2 For 校記 #3 for noteref
     for (id, href) in bk.text_iter():
         step = 1
         modified = False
@@ -32,34 +30,36 @@ def run(bk):
             innerText = e.text
             if innerText is None or innerText == "":
                 continue
-            if e.text == "■校記":
+            if e.text == "■校記" or e.text == "■校注":
                 step = 2
-            if e.text == "■注釋":
+            if e.text == "■注釋" or e.text == "■註釋":
                 step = 3
             if step == 1:
                 original = innerText
-                innerText = re.sub(r'（校記(\d+)）',r'<a class="duokan-footnote footnote2" href="#fx\1"  id="fxref\1">\1</a>',innerText)
-                innerText = re.sub(r'（注釋(\d+)）',r'<a class="duokan-footnote footnote1" href="#fn\1"  id="fnref\1">\1</a>',innerText)
-                if original  != innerText:
+                innerText = re.sub(r'（校記(\d+)）', r'<a class="duokan-footnote footnote2" href="#fx\1"  id="fxref\1">\1</a>', innerText)
+                innerText = re.sub(r'（校注(\d+)）', r'<a class="duokan-footnote footnote2" href="#fx\1"  id="fxref\1">\1</a>', innerText)
+                innerText = re.sub(r'（注釋(\d+)）', r'<a class="duokan-footnote footnote1" href="#fn\1"  id="fnref\1">\1</a>', innerText)
+                innerText = re.sub(r'（註釋(\d+)）', r'<a class="duokan-footnote footnote1" href="#fn\1"  id="fnref\1">\1</a>', innerText)
+                if original != innerText:
                     modified = True
                     # print(innerText)
-                    e.getparent().replace(e, etree.XML("<"+e.tag+">"+innerText+"</"+e.tag+">"))
+                    e.getparent().replace(e, etree.XML("<" + e.tag + ">" + innerText + "</" + e.tag + ">"))
             if step == 2:
                 match = re.search("^(\d+)", innerText)
                 if match is not None:
-                    innerText = re.sub(r'^(\d+)',r'<a class="duokan-footnote-item footnote-item2" href="#fxref\1"  id="fx\1">\1</a>',innerText)
+                    innerText = re.sub(r'^(\d+)', r'<a class="duokan-footnote-item footnote-item2" href="#fxref\1"  id="fx\1">\1</a>', innerText)
                     # print("校記", innerText)
-                    e.getparent().replace(e, etree.XML("<"+e.tag+">"+innerText+"</"+e.tag+">"))
+                    e.getparent().replace(e, etree.XML("<" + e.tag + ">" + innerText + "</" + e.tag + ">"))
             if step == 3:
                 match = re.search("^(\d+)", innerText)
                 if match is not None:
-                    innerText = re.sub(r'^(\d+)',r'<a class="duokan-footnote-item footnote-item1" href="#fnref\1"  id="fn\1">\1</a>',innerText)
+                    innerText = re.sub(r'^(\d+)', r'<a class="duokan-footnote-item footnote-item1" href="#fnref\1"  id="fn\1">\1</a>', innerText)
                     # print("注釋 ", innerText)
-                    e.getparent().replace(e, etree.XML("<"+e.tag+">"+innerText+"</"+e.tag+">"))
-        for a in doc.xpath("//*[contains(@class, 'duokan-footnote footnote1')]"):
-            a.attrib['epub:type']= "noteref"
-        for a in doc.xpath("//*[contains(@class, 'duokan-footnote-item footnote-item1')]"):
-            a.getparent().attrib['epub:type']= "footnote"
+                    e.getparent().replace(e, etree.XML("<" + e.tag + ">" + innerText + "</" + e.tag + ">"))
+        # for a in doc.xpath("//*[contains(@class, 'duokan-footnote footnote1')]"):
+        #     a.attrib['epub:type'] = "noteref"
+        # for a in doc.xpath("//*[contains(@class, 'duokan-footnote-item footnote-item1')]"):
+        #     a.getparent().attrib['epub:type'] = "footnote"
         if modified:
             head = doc.xpath("//*[local-name() = 'head']")[0]
             link = etree.Element("link")
