63b70fe0906f7e1f2cf9761d0837d223110044a4
林檎 <apple@apples-MacBook-Pro.local>
2017-04-13 15:04:56 +0800

    Improve CSS

--- vertical-rtl/plugin.py
+++ vertical-rtl/plugin.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-import re
 import sys
 from lxml import etree, html
 
@@ -13,38 +12,66 @@ def run(bk):
         html_original = bk.readfile(id)
         doc = html.fromstring(html_original.encode("utf-8"))
         for link in doc.xpath("//*[local-name() = 'link']"):
-            if link['href'] == "../Styles/t2bv2l.css":
-                modified= False
+            if link.attrib['href'] == "../Styles/t2bv2l.css":
+                modified = False
                 break
 
         if modified:
             count += 1
             head = doc.xpath("//*[local-name() = 'head']")[0]
-            link = etree.SubElement(head, "link", attrib={'href': "../Styles/t2bv2l.css",
-                                                          'rel': "stylesheet",
-                                                          'type': "text/css"
-                                                         })
+            link = etree.SubElement(head,
+                                    "link",
+                                    attrib={'href': "../Styles/t2bv2l.css",
+                                            'rel': "stylesheet",
+                                            'type': "text/css"
+                                           })
             print("Modified File : ", id)
-            bk.writefile(id, etree.tostring(doc, encoding="utf-8", xml_declaration=True).decode('utf8'))
+            bk.writefile(id,
+                         etree.tostring(
+                             doc,
+                             encoding="utf-8",
+                             xml_declaration=True).decode('utf8'))
 
 # css
     if count > 0:
         cssdata = '''
 html{
     direction:rtl;
+    -ms-writing-mode: tb-rl;
+    -epub-writing-mode: vertical-rl;
+    -webkit-writing-mode: vertical-rl;
+    writing-mode: vertical-rl;
 }
 body {
     direction:ltr;
+    word-break: normal;
+    text-align: justify;
+    text-justify: inter-ideograph;
+    vertical-align: baseline;
+    word-wrap: break-word;
     line-break: normal;
     -epub-line-break: normal;
     -webkit-line-break: normal;
-    writing-mode: tb-rl;
-    -epub-writing-mode: vertical-rl;
-    -webkit-writing-mode: vertical-rl;
-    -ms-writing-mode: vertical-rl;
-    text-orientation: mixed;
-    -webkit-text-orientation: mixed;
-    -epub-text-orientation: mixed;
+    text-orientation: upright;
+    -webkit-text-orientation: upright;
+    -epub-text-orientation: upright;
+}
+.tcy {
+  -epub-text-combine: horizontal;
+  -webkit-text-combine: horizontal;
+  -ms-text-combine-horizontal: all;
+  text-combine-horizontal: all;
+  text-combine-upright: all;
+}
+.upright {
+  -epub-text-orientation: rotate-right;
+  -epub-text-orientation: upright;
+  -webkit-text-orientation: upright;
+  -epub-text-combine: horizontal;
+  -webkit-text-combine: horizontal;
+  -ms-text-combine-horizontal: all;
+  text-combine-horizontal: all;
+  text-combine-upright: all;
 }'''
         basename = "t2bv2l.css"
         uid = "t2bv2lcss"
