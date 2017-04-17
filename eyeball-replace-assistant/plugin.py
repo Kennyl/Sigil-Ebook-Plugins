1a44fe8e0c6a8c4a452793bc08ca1e44ba459dfb
林檎 <apple@apples-MacBook-Pro.local>
2017-04-17 15:12:39 +0800

    Output File can be optional

--- eyeball-replace-assistant/plugin.py
+++ eyeball-replace-assistant/plugin.py
@@ -13,6 +13,7 @@ import PyQt5.QtCore
 lineEditPrompt = "String to Find (seperated in Spacebar)"
 defaultInput =  "幹 乾 干 髮 里 裡 衝 沖 制 製 準"
 regexpCondition = r'.{0,3}'
+outputAsFile = True
 
 class askSetting(QWidget):
 
@@ -29,14 +30,19 @@ class askSetting(QWidget):
 
       layout = QVBoxLayout()
 
-    #   self.buttons = {}
+      self.buttons = {}
       self.lineedits = {}
 
       for key in items.keys():
-        layout.addWidget(QLabel(key))
-        self.lineedits[key] = QLineEdit()
-        self.lineedits[key].setText(items[key])
-        layout.addWidget(self.lineedits[key])
+        if type(items[key]) is bool:
+            self.buttons[key] = QCheckBox(key)
+            self.buttons[key].setChecked(items[key])
+            layout.addWidget(self.buttons[key])
+        else:
+            layout.addWidget(QLabel(key))
+            self.lineedits[key] = QLineEdit()
+            self.lineedits[key].setText(items[key])
+            layout.addWidget(self.lineedits[key])
 
       self.btn = QPushButton('OK', self)
       self.btn.clicked.connect(lambda:(self.bye(items)))
@@ -50,8 +56,8 @@ class askSetting(QWidget):
    def bye(self, items):
        for key in self.lineedits.keys():
            self.items[key] = self.lineedits[key].text()
-    #    for key in self.buttons.keys():
-    #        self.items[key] = self.buttons[key].isChecked()
+       for key in self.buttons.keys():
+           self.items[key] = self.buttons[key].isChecked()
        self.close()
        self.app.exit(1)
 
@@ -62,7 +68,8 @@ def run(bk):
     if sys.platform == "darwin":
         print("Plugin using PyQt5, bundled Python may not work")
 
-    items = {lineEditPrompt:defaultInput}
+    items = {lineEditPrompt:defaultInput,
+             "Output As File":outputAsFile}
 
     app = QApplication(sys.argv)
     ask = askSetting(app=app, items=items)
@@ -105,20 +112,25 @@ def run(bk):
     print('\n\n===================================================\n')
     print("File saved as Text/_eyeball-replace-assistant*.html")
     print('===================================================\n\n')
-    text = ""
+    breakdown_text = ""
     for word in sorted(result_dicts):
         print("%s (%s) " % (word, len(result_dicts[word])))
         print('===================================================')
+        breakdown_text += '\n===================================================\n'
+        breakdown_text += "%s (%s) " % (word, len(result_dicts[word]))
+        breakdown_text += '\n===================================================\n'
         for pattern in sorted(result_dicts[word]):
             # line = "Found %s as pattern %s in %s ." % (pattern, word, result_dicts[word][pattern])
             line = pattern
+            breakdown_text += pattern + '\n'
             print(line)
-            text += line + "\n"
+            # breakdown_text += line + "\n"
         print('===================================================')
     print('\n            Done. Copy as you need\n')
     print('===================================================\n\n')
-
-    if text != "":
+    breakdown_text += '\n===================================================\n'
+    text = ""
+    if breakdown_text != "":
         #revesed list
         reversed_dicts = {}
         for word in result_dicts:
@@ -137,7 +149,9 @@ def run(bk):
         '(Tab Seperated Version) as text file '
         '<a href="Misc/_eyeball-replace-assistant.tsv.txt">'
         'Misc/_eyeball-replace-assistant.tsv.txt</a> <br/><hr/>')
-
+        body += ('Breakdown'
+        '<a href="Misc/_eyeball-replace-assistant.txt">'
+        'Misc/_eyeball-replace-assistant.txt</a> <br/><hr/>')
         for href in sorted(reversed_dicts):
             body += '<br/><hr/><a href="%s">%s</a><br/><hr/>\n' % (href, href)
             for word in sorted(reversed_dicts[href]):
@@ -148,12 +162,20 @@ def run(bk):
                         % (word, html.escape(r''.join(pattern))))
             body += "<br/><hr/>\n"
 
-        # bk.addotherfile('eyeball-replace-assistant.txt',text)
-        bk.addotherfile('_eyeball-replace-assistant.html',
-                        '<html><body>'+body+'</body></html>')
-        bk.addotherfile('_eyeball-replace-assistant.tsv.txt',
-                        tsv)
-#varible (text) hasn't used 
+        if outputAsFile:
+            print('File Output\n===================================')
+            print('Text/_eyeball-replace-assistant*.html')
+            print('Misc/_eyeball-replace-assistant*.tsv.txt')
+            print('Misc/_eyeball-replace-assistant*.txt')
+            print('===================================')
+            # bk.addotherfile('eyeball-replace-assistant.txt',text)
+            bk.addotherfile('_eyeball-replace-assistant.html',
+                            '<html><body>'+body+'</body></html>')
+            bk.addotherfile('_eyeball-replace-assistant.tsv.txt',
+                            tsv)
+            bk.addotherfile('_eyeball-replace-assistant.txt',
+                            breakdown_text)
+#varible (text) hasn't used
     return 0
 
 
